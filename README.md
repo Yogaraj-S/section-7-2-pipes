cc7b5558b604638b7a06d54e46f10ca627c36632
Yogaraj-S <yogaraj@ideas2it.com>
2020-03-09 14:28:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# section-7-2-pipes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/section-7-2-pipes)
---
+++ src/src/app/home/task.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    id: Number,
+    taskName: String,
+    creator: String,
+    assignee: String,
+    status: String,
+    priority: String,
+    details: String
+}
